e2460b205f9da17db679dbda23fde853c9f60514
EmanTurk <143930452+EmanTurk@users.noreply.github.com>
2023-11-24 17:48:15 +0200

    Update App.jsx

--- reactVite-Assignments/src/App.jsx
+++ reactVite-Assignments/src/App.jsx
@@ -1,24 +1,18 @@
 import React from 'react'
 import './App.css'
 
-// function PrintApp (){
-// const data = ["hello", "world"];
-// const number1 = 5;
-// const number2 =6;
-// const string = "I love React!";
+function PrintApp (){
+const data = ["hello", "world"];
+const number1 = 5;
+const number2 =6;
+const string = "I love React!";
 
-// return (
-//     <div className='mainDiv'>
-//       <div>{`${data[0].charAt(0).toUpperCase() + data[0].slice(1)} ${data[1].charAt(0).toUpperCase() + data[1].slice(1)}`}</div>
-//       <div>{`${number1} + ${number2} = ${number1 + number2}`}</div>
-//       <div>{`The string’s length is ${string.length}`}</div>
-//     </div>
-//   );
-// }
-// export default PrintApp;
-function App() {
-    return <div>Hello World!</div>;
-  }
-  
-  export default App;
-  
+return (
+    <div className='mainDiv'>
+      <div>{`${data[0].charAt(0).toUpperCase() + data[0].slice(1)} ${data[1].charAt(0).toUpperCase() + data[1].slice(1)}`}</div>
+      <div>{`${number1} + ${number2} = ${number1 + number2}`}</div>
+      <div>{`The string’s length is ${string.length}`}</div>
+    </div>
+  );
+
+}
